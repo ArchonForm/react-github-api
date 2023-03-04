b238a1cdf72a1ab41251e09afbfffd0a64896987
Anton Pogulyaev <a.v.pogulyaev@gmail.com>
2023-03-04 13:13:33 +0500

    add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import { HomePage } from './pages/HomePage'
 import { FavouritesPage } from './pages/FavouritesPage'
+import { Navigation } from './components/Navigation'
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<HomePage />}></Route>
-      <Route path='/favourites' element={<FavouritesPage />}></Route>
-    </Routes>
+    <>
+      <Navigation></Navigation>
+      <Routes>
+        <Route path='/' element={<HomePage />}></Route>
+        <Route path='/favourites' element={<FavouritesPage />}></Route>
+      </Routes>
+    </>
   )
 }
 
