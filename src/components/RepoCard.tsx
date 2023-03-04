8c26384f4ada47ba7c6aa7dd1cc3c538c3e69e98
Anton Pogulyaev <a.v.pogulyaev@gmail.com>
2023-03-04 15:49:10 +0500

    implement interraction with favourites

--- src/components/RepoCard.tsx
+++ src/components/RepoCard.tsx
@@ -1,6 +1,26 @@
+import React, { useState } from 'react'
+import { useActions } from '../hooks/actions'
+import { useAppSelector } from '../hooks/redux'
 import { Repo } from '../models'
 
 export function RepoCard({ repo }: { repo: Repo }) {
+  const { addFavourites, removeFavourites } = useActions()
+  const { favourites } = useAppSelector(state => state.github)
+
+  const [isFav, setIsFav] = useState(favourites.includes(repo.html_url))
+
+  const addToFavourite = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+    addFavourites(repo.html_url)
+    setIsFav(true)
+  }
+
+  const removeFromFavourite = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+    removeFavourites(repo.html_url)
+    setIsFav(false)
+  }
+
   return (
     <div className='border py-3 px-5 rounded cursor-pointer mb-2 hover:shadow-md hover:bg-gray-100 transition-all'>
       <a href={repo.html_url} target='_blank'>
@@ -10,6 +30,24 @@ export function RepoCard({ repo }: { repo: Repo }) {
           Watchers: <span className='font-bold'>{repo.watchers}</span>
         </p>
         <p className='text-sm font-thin'>{repo?.description}</p>
+
+        {!isFav && (
+          <button
+            className='py-2 px-4 bg-yellow-400 rounded hover:shadow-md transition-all mr-3'
+            onClick={addToFavourite}
+          >
+            Add
+          </button>
+        )}
+
+        {isFav && (
+          <button
+            className='py-2 px-4 bg-red-400 rounded hover:shadow-md transition-all'
+            onClick={removeFromFavourite}
+          >
+            Remove
+          </button>
+        )}
       </a>
     </div>
   )
--- src/pages/FavouritesPage.tsx
+++ src/pages/FavouritesPage.tsx
@@ -1,3 +1,23 @@
+import { useAppSelector } from '../hooks/redux'
+
 export function FavouritesPage() {
-  return <div>FavouritesPage</div>
+  const { favourites } = useAppSelector(state => state.github)
+
+  if (favourites.length == 0) {
+    return <p className='text-center'>No items.</p>
+  }
+
+  return (
+    <div className='flex justify-center pt-10 mx-auto h-screen w-screen'>
+      <ul className='list-none'>
+        {favourites.map(link => (
+          <li key={link}>
+            <a href={link} target='_blank'>
+              {link}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
