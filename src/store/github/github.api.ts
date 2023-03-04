c77e1762f1406ba086760eb7afa1d94b24b14b41
Anton Pogulyaev <a.v.pogulyaev@gmail.com>
2023-03-04 14:23:04 +0500

    implement search block

--- src/store/github/github.api.ts
+++ src/store/github/github.api.ts
@@ -6,6 +6,7 @@ export const githubApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://api.github.com',
   }),
+  refetchOnFocus: true,
   endpoints: builder => ({
     searchUsers: builder.query<User[], string>({
       query: (search: string) => ({
