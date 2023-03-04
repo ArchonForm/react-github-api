7eb11c6c7898a4735bfdc87f59a2e3fe665da03e
Anton Pogulyaev <a.v.pogulyaev@gmail.com>
2023-03-04 13:44:14 +0500

    add users fetching

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,9 @@
+import { useSearchUsersQuery } from '../store/github/github.api'
+
 export function HomePage() {
+  const { isLoading, isError, data } = useSearchUsersQuery('archon')
+
+  console.log(data)
+
   return <div>HomePage</div>
 }
---
+++ src/store/github/github.api.ts
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { ServerResponse, User } from '../../models'
+
+export const githubApi = createApi({
+  reducerPath: 'github/api',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://api.github.com',
+  }),
+  endpoints: builder => ({
+    searchUsers: builder.query<User[], string>({
+      query: (search: string) => ({
+        url: 'search/users',
+        params: {
+          q: search,
+          per_page: 10,
+        },
+      }),
+      transformResponse: (response: ServerResponse<User>) => response.items,
+    }),
+  }),
+})
+
+export const { useSearchUsersQuery } = githubApi
