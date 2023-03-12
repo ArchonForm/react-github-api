8c18c72f9ce2afc496adcf7557c15046fc0ad462
Anton Pogulyaev <a.v.pogulyaev@gmail.com>
2023-03-12 13:19:42 +0500

    add deployment scripts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import { HomePage } from './pages/HomePage'
 import { FavouritesPage } from './pages/FavouritesPage'
 import { Navigation } from './components/Navigation'
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />}></Route>
         <Route path='/favourites' element={<FavouritesPage />}></Route>
+        <Route path='/*' element={<Navigate to='/' replace />}></Route>
       </Routes>
     </>
   )
